765e21c27e90fe3bfbfb5e88da66b1fb6a36734e
orbitapurificante-design <orbitapurificante@gmail.com>
2026-04-06 04:41:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
-const CACHE_NAME = 'marinha-v1';
+const CACHE_NAME = 'marinha-v2';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './logoDM.jpg'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
